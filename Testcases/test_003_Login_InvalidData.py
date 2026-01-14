4927fa35bf87dbec83d67eacc60d41a006cbbbd7
mahnisha17 <mahnisha.acharya17@gmail.com>
2026-01-14 14:22:02 +0545

    reformed code

--- Testcases/test_003_Login_InvalidData.py
+++ Testcases/test_003_Login_InvalidData.py
@@ -1,14 +1,17 @@
-from Base.InitiateDriver import startBrowser,closeBrowser
-
+#from Base.InitiateDriver import startBrowser,closeBrowser
+from selenium.webdriver import Chrome
 def test_InvalidLogin():
     
-    driver = startBrowser()
+    #driver = startBrowser()
+    driver=Chrome()
+    driver.get("https://facebook.com/login")
     
     driver.find_element('xpath',"//input[@name='email']").send_keys("1hello")
     driver.find_element('xpath',"//input[@name='pass']").send_keys("4567")
     driver.find_element('xpath',"//button[@name='login']").click()
     
-    closeBrowser()
+    #closeBrowser()
+    driver.close()
     
     
     
